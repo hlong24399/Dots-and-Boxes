fce7791344d3d0c0d660b4c45fd80b83e31fc195
LoganNguyen <54257351+hlong24399@users.noreply.github.com>
2019-10-17 00:22:25 -0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <vector>
 #include <SFML/Graphics.hpp>
-const int screen_x = 600;
-const int screen_y = 600;
+const int screen_x = 1000;
+const int screen_y = 1000;
 using namespace sf;
+struct Choice {
+	int a;
+	int b;
+};
 class Board {
-private:
+public:
 	std::vector<CircleShape>circle;
+	std::vector<Choice>choice;
 public:
 	Board(int size);
+public:
 	std::vector<CircleShape> getCircle();//return vector of dots
+	void setChoice(int a, int b);
+	void PrintChoice();
 };
