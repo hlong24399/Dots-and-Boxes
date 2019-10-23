5dce5c75890a62ba7225c6c37c41c0686be8d572
LoganNguyen <54257351+hlong24399@users.noreply.github.com>
2019-10-22 19:52:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,33 +2,35 @@
 #include <iostream>
 #include <vector>
 #include "Board.h"
+#include "Box.h"
 #include <cmath>
 using namespace sf;
-
 int main() {
 
-	RenderWindow window(VideoMode(screen_x, screen_y), " Dots and Boxes ", Style::Default);
-	window.setPosition(Vector2i(700,100));
-	window.setFramerateLimit(144);
+	RenderWindow window(VideoMode(screen_size, screen_size), " Dots and Boxes ", Style::Default);
+	window.setPosition(Vector2i(1000, 10));
+	window.setFramerateLimit(60);
 	int edge{}; ///////Size of the game
 	std::cout << " Enter the number of dots you want on an edge: ";
-	
-	//std::cin >> edge; ////Input length of edge
-	edge = 3;
-	
-	Board board(edge); ////Game starts
-	while (window.isOpen()) {
 
+	//std::cin >> edge; ////Input length of edge
+	edge = 5;
+
+	Board board(edge,window); ////Game starts
+	while (window.isOpen()) {
+		board.LineSelect();
+		
 		Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		window.clear(Color::White);
-		//window.draw(vertex);
+		window.clear();
+		board.drawLines(window);
 		window.display();
-		
+
 	}
 	return 0;
 }
+
 
