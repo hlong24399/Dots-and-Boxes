074a35c8a5fc41f9cafc2b72b0f68dab8f27f1b8
LoganNguyen <54257351+hlong24399@users.noreply.github.com>
2019-10-26 00:03:27 -0700

    Update Box.cpp

--- Box.cpp
+++ Box.cpp
@@ -1,5 +1,28 @@
 #include "Box.h"
 #include "Board.h"
 using namespace sf;
+////Get those edges
+void Edge::inputEdges(int a, int b)
+{
+	Edges.push_back(std::make_pair(a, b));
+}
+
+////Get the size to future use----------------------------------------
+void Edge::setSize(int size, int totalLines)
+{
+	this->size = size;
+	this->totalLines = totalLines;
+}
+
+////This function will check if the edges is valid for box or not.
+bool Edge::in(int a, int b )  {
+	std::pair<int, int> test_pair(std::make_pair(a, b));
+	for (auto i : Edges) {
+		if (test_pair == i) { 
+			return true; }
+	}
+	return false;
+}
+
 
 
