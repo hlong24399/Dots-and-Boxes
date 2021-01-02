e8c4ae2a301b6534fa7c889e3488420c06246e0e
Hoang Long <hlong24399@gmail.com>
2021-01-01 22:00:48 -0800

    updated

--- main.cpp
+++ main.cpp
@@ -77,8 +77,3 @@ int main() {
 	}
 	return 0;
 }
-
-
-void welcome() {
-	std::cout << "welcome to Dots and Boxes game, you have to complete more squares on the boards than the bots to win the game. Start by choosing your board size, and make your move by choosing the two dots to form an edge!" << std::endl;
-}
